b6bb58f898587753bfcda27e570e643186f7d718
Ishwak Rahman <ishwak9526152121@gmail.com>
2021-10-22 23:35:23 +0530

    changed images for the bg carousel
    images changed

--- js/templatemo-script.js
+++ js/templatemo-script.js
@@ -1,5 +1,5 @@
 const initBg = (autoplay = true) => {
-    const bgImgsNames = ['diagoona-bg-1.jpg', 'diagoona-bg-2.jpg', 'diagoona-bg-3.jpg'];
+    const bgImgsNames = ['m-1.jpg', 'm-2.jpg', 'm-3.jpg'];
     const bgImgs = bgImgsNames.map(img => "img/" + img);
 
     $.backstretch(bgImgs, {duration: 4000, fade: 500});
